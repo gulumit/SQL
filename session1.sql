3ab9b651fcad1f3ef388ac45eb47daaedbad34cf
gulumit <umityilmazgul@gmail.com>
2021-12-09 00:20:32 +0300

    first commit

---
+++ session1.sql
@@ -0,0 +1,25 @@
+/*tracks tablosundaki track isimlerini sorgula*/
+
+SELECT name FROM tracks;
+/*tracks tablosundaki besteci ve şarkı isimlerini sorgula*/
+
+SELECT Composer, name FROM tracks;
+/*tracks tablosundaki tüm bilgileri sorgula*/
+
+SELECT * FROM tracks;
+
+SELECT Composer FROM tracks
+/*tekrarsızları bulmak için*/
+SELECT DISTINCT Composer FROM tracks
+SELECT DISTINCT AlbumId,MediaTypeId FROM tracks
+
+/*=====================================================
+                           WHERE
+======================================================*/						   
+SELECT name FROM tracks WHERE Composer='Jimi Hendrix';
+
+/* invoices tablosundan total değeri 10dan büyük olan faturalar sorgula*/
+SELECT * 
+FROM invoices 
+WHERE total>10;
+
